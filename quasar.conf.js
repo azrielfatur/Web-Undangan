c45a3febfe45c0a82a92f50762fe0f063936c91e
azrielfatur <halfhour.id@gmail.com>
2022-03-13 19:12:29 +0700

    edit quasar conf

--- quasar.conf.js
+++ quasar.conf.js
@@ -104,7 +104,7 @@ module.exports = configure(function (ctx) {
 
     // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
-      pwa: false,
+      pwa: true,
 
       // manualStoreHydration: true,
       // manualPostHydrationTrigger: true,
--- src/pages/ComingSoon.vue
+++ src/pages/ComingSoon.vue
@@ -2,7 +2,7 @@
   <q-layout view="hHh lpR fFf" class="tw-font-poppins tw-overflow-x-hidden">
     <div class="tw-w-full tw-h-screen tw-flex tw-items-center ">
       <div class="tw-mx-auto">
-        <p class="tw-text-2xl lg:tw-text-4xl tw-font-black">Anywedd | Coming Soon</p>
+        <p class="tw-text-4xl tw-font-black">Anywedd | Coming Soon</p>
       </div>
     </div>
   </q-layout>
